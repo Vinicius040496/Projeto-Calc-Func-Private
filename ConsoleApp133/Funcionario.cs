269c56e6014a4ccd08a44fb45a33ff6482e0f35d
Vinicius040496 <vinicius.almeida2011@gmail.com>
2024-06-07 16:10:19 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp133/Funcionario.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp133
+{
+    internal class Funcionario
+    {
+        private string _nome;
+        public int HorasTrabalhada { get; private set; }
+        public double ValorHora { get; private set; }
+
+        public Funcionario(string nome, int horastrabalhadas, double valorhora)
+        {
+            _nome = nome;
+            HorasTrabalhada = horastrabalhadas;
+            ValorHora = valorhora;
+        }
+
+        public double SalarioMes()
+        {
+            return HorasTrabalhada * ValorHora;
+        }
+        public string Nome       
+           {   
+            get { return _nome;
+            }
+            set
+            {
+                if (value != null && value.Length > 2)
+                {
+                    Nome = value;
+                }
+            }
+        }
+        public override string ToString()
+        {
+            return "Funcionario: " + Nome
+                + ", Horas trabalhadas nesse mês: " + HorasTrabalhada + " Horas" 
+                + ", valor hora funcionario: " + ValorHora.ToString("f2 ")
+                + " Salario Mês: " + SalarioMes().ToString("f2");
+        }
+    }
+}
---
+++ ConsoleApp133/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp133
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ficha funcionario");
+
+            Console.WriteLine("Nome do funcionario: ");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Horas mensal trabalhadas: ");
+            int horastrabalhadas = int.Parse(Console.ReadLine());
+            Console.WriteLine("Valor hora do funcionario;");
+            double valorhora = double.Parse(Console.ReadLine());
+   
+            Funcionario F = new Funcionario(nome,horastrabalhadas,valorhora);
+            F.SalarioMes();
+
+            Console.WriteLine(F);
+            Console.ReadLine();
+        }
+    }
+}
